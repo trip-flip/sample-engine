d74439b1cd2a8a3bc8193e7370e54e2f264dfe45
trip-flip <thomasbaugher@outlook.com>
2021-08-24 14:42:59 -0400

    Finshed on_create() definition, switched to ref

--- gl-wrap-core/src/scriptable.rs
+++ gl-wrap-core/src/scriptable.rs
@@ -2,7 +2,7 @@ use crate::ecs::Entity;
 
 pub trait Scriptable {
     fn create() -> Self;
-    fn on_create() {}
-    fn on_update(&mut self, _entity: *mut Entity) {}
+    fn on_create(&mut self, _entity: &mut Entity) {}
+    fn on_update(&mut self, _entity: &mut Entity) {}
     fn on_destroy() {}
 }
