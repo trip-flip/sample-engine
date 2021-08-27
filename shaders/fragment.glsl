e7f15991af0f5ec1eed7332217368412fd284ef2
trip-flip <thomasbaugher@outlook.com>
2021-08-27 13:30:12 -0400

    Defaults to colors if no texture

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -3,9 +3,14 @@
 out vec4 frag_color;
 
 in vec2 t_pos;
+in vec3 v_pos;
 
 uniform sampler2D t_data;
 
 void main() {
-    frag_color = texture(t_data, t_pos);
+    vec4 color = texture(t_data, t_pos);
+    if (color.x == 0.0 && color.y == 0.0 && color.z == 0.0)
+        color = vec4(v_pos, 1);
+
+    frag_color = color;
 }
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -4,6 +4,7 @@ layout (location = 0) in vec3 a_v_pos;
 layout (location = 1) in vec2 a_t_pos;
 
 out vec2 t_pos;
+out vec3 v_pos;
 
 uniform mat4 model = mat4(1);
 uniform mat4 proj = mat4(1);
@@ -11,4 +12,5 @@ uniform mat4 proj = mat4(1);
 void main() {
     gl_Position = proj * model * vec4(a_v_pos, 1.0);
     t_pos = a_t_pos;
+    v_pos = a_v_pos;
 }
